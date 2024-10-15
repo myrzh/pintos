1680923e7f95c2a4005841d220c6ef218e3a7f76
Ivan Shtarev <ishtarev@outlook.com>
2024-10-16 00:02:54 +0300

    add comments and refactor a little

--- src/tests/threads/narrow-bridge.c
+++ src/tests/threads/narrow-bridge.c
@@ -16,22 +16,23 @@ enum operation
 
 enum place_mode
 {
-  one = 0,
-  two_equal = 1,
-  two_mixed = 2
+  one = 0,       // One car
+  two_equal = 1, // Two cars with equal priorities
+  two_mixed = 2  // Two cars with different priorities
 };
 
 struct semaphore norm_left_sema, norm_right_sema, emer_left_sema, emer_right_sema;
-struct semaphore general_sema;
+struct semaphore mutex_sema; // Control bridge access
 int norm_left, norm_right, emer_left, emer_right;
-int was_started;
-int now_crossing;
-int cars_overall;
-int cars_unblocked;
+int was_started;    // Was the first direction chosen
+int now_crossing;   // Number of cars on the bridge
+int cars_overall;   // Number of waiting cars
+int cars_unblocked; // Number of ready-to-move cars
 enum car_priority current_car;
 enum car_direction current_direction;
 
-void signal_sema() {
+/** Unlock selected semaphore */
+void car_sema_up() {
   if (current_car == car_normal && current_direction == dir_left) {
     sema_up(&norm_left_sema);
   } else if (current_car == car_emergency && current_direction == dir_left) {
@@ -43,33 +44,48 @@ void signal_sema() {
   }
 }
 
+/** Lock selected semaphore */
+void car_sema_down(enum car_priority prio, enum car_direction dir) {
+  if (prio == car_normal && dir == dir_left) {
+    sema_down(&norm_left_sema);
+  } else if (prio == car_emergency && dir == dir_left) {
+    sema_down(&emer_left_sema);
+  } else if (prio == car_normal && dir == dir_right) {
+    sema_down(&norm_right_sema);
+  } else if (prio == car_emergency && dir == dir_right) {
+    sema_down(&emer_right_sema);
+  }
+}
+
+/** Place one car, two equal or two mixed cars */
 void place_car(enum place_mode mode) {
   switch (mode) {
     case one:
-      sema_down(&general_sema);
-      signal_sema();
-      sema_down(&general_sema);
+      sema_down(&mutex_sema); // Reserve one place on the bridge
+      car_sema_up();          // Unlock current car semaphore
+      sema_down(&mutex_sema); // Reserve another place on the bridge (so that others wouldn't drive)
       now_crossing += 1;
       current_car = car_normal;
       break;
     case two_equal:
-      signal_sema();
-      sema_down(&general_sema);
-      signal_sema();
-      sema_down(&general_sema);
+      car_sema_up();
+      sema_down(&mutex_sema);
+      car_sema_up();
+      sema_down(&mutex_sema);
       now_crossing += 2;
       current_car = car_normal;
       break;
     case two_mixed:
+      // Place emergency in the first place, then normal car
       if (current_direction == dir_left) {
         sema_up(&emer_left_sema);
-        signal_sema();
+        car_sema_up();
       } else if (current_direction == dir_right) {
         sema_up(&emer_right_sema);
-        signal_sema();
+        car_sema_up();
       }
-      sema_down(&general_sema);
-      sema_down(&general_sema);
+      sema_down(&mutex_sema);
+      sema_down(&mutex_sema);
       now_crossing += 2;
       break;
     default:
@@ -77,6 +93,7 @@ void place_car(enum place_mode mode) {
   }
 }
 
+/** Choose the most appropriate car(s) and place */
 void choose_and_place() {
   int emer_count, norm_count;
   
@@ -88,22 +105,23 @@ void choose_and_place() {
     norm_count = norm_right;
   }
 
-  if (emer_count >= 2) {
+  if (emer_count >= 2) { // If we have two emers, place them together
     current_car = car_emergency;
     place_car(two_equal);
-  } else if (emer_count == 1 && norm_count >= 1) {
+  } else if (emer_count == 1 && norm_count >= 1) { // If we have one emer and at least one norm, place them together
     place_car(two_mixed);
-  } else if (emer_count == 1 || norm_count >= 2) {
+  } else if (emer_count == 1 || norm_count >= 2) { // If we have one emer or at least two norms
     if (emer_count == 1) {
       current_car = car_emergency;
     }
-    place_car(emer_count == 1 ? one : two_equal);
-  } else {
-    sema_down(&general_sema);
-    sema_down(&general_sema);
+    place_car(emer_count == 1 ? one : two_equal);  // Place one emer or two norms
+  } else {                  // If no cars in current direction ...
+    sema_down(&mutex_sema); // ... block the bridge anyway
+    sema_down(&mutex_sema);
   }
 }
 
+/** Update counts of each type of the cars */
 void update_counts(enum car_priority prio, enum car_direction dir, enum operation oper) {
   int *count = NULL;
   if (prio == car_normal && dir == dir_left) {
@@ -123,32 +141,21 @@ void update_counts(enum car_priority prio, enum car_direction dir, enum operatio
   }
 }
 
-void car_sema_down(enum car_priority prio, enum car_direction dir) {
-  if (prio == car_normal && dir == dir_left) {
-    sema_down(&norm_left_sema);
-  } else if (prio == car_emergency && dir == dir_left) {
-    sema_down(&emer_left_sema);
-  } else if (prio == car_normal && dir == dir_right) {
-    sema_down(&norm_right_sema);
-  } else if (prio == car_emergency && dir == dir_right) {
-    sema_down(&emer_right_sema);
-  }
-}
-
 /** Called before test. Can initialize some synchronization objects. */
 void narrow_bridge_init(void) {
-  sema_init(&general_sema, 1);
+  sema_init(&mutex_sema, 1);
   sema_init(&norm_left_sema, 0);
   sema_init(&emer_left_sema, 0);
   sema_init(&norm_right_sema, 0);
   sema_init(&emer_right_sema, 0);
 }
 
+/** Called every time the new car arrives */
 void arrive_bridge(enum car_priority prio, enum car_direction dir) {
   update_counts(prio, dir, increment);
-  thread_yield();
+  thread_yield(); // Give another cars the opportunity to reach this code section
 
-  if (was_started == 0) {
+  if (was_started == 0) { // Determine first direction and count cars (once)
     if (emer_left > emer_right) {
       current_direction = dir_left;
     } else if (emer_left < emer_right) {
@@ -162,40 +169,41 @@ void arrive_bridge(enum car_priority prio, enum car_direction dir) {
     was_started = 1;
   }
 
-  if (cars_unblocked > 1) {
+  if (cars_unblocked > 1) {   // If more than one unblocked car ...
     cars_unblocked--;
-    car_sema_down(prio, dir);
+    car_sema_down(prio, dir); // ... send it to queue
   }
-  if (cars_unblocked == 1) {
+  if (cars_unblocked == 1) {  // If one unblocked car is left ...
     cars_unblocked--;
-    sema_up(&general_sema);
-    sema_up(&general_sema);
-    choose_and_place();
-    car_sema_down(prio, dir);
+    sema_up(&mutex_sema);     // ... clear bridge ...
+    sema_up(&mutex_sema);
+    choose_and_place();       // ... choose and place the most appropriate car(s) ...
+    car_sema_down(prio, dir); // ... and block current car
   }
 }
 
+/** Called every time the car needs to exit the bridge (already crossed) */
 void exit_bridge(enum car_priority prio, enum car_direction dir) {
   update_counts(prio, dir, decrement);
-  cars_overall = norm_left + emer_left + norm_right + emer_right;
+  cars_overall = norm_left + emer_left + norm_right + emer_right; // Count waiting cars
   now_crossing--;
 
-  if (cars_overall != 0 && now_crossing % 2 == 0) {
-    // Check if emergency cars are present and prioritize them
+  if (cars_overall != 0 && now_crossing % 2 == 0) { // If we still have waiting cars and the bridge is free
+    // Change direction if there are emers in the other direction
     if ((current_direction == dir_left && emer_right == 0 && emer_left > 0) ||
         (current_direction == dir_right && emer_left == 0 && emer_right > 0)) {
       current_direction = (current_direction == dir_left) ? dir_right : dir_left;
     } else {
-      // Check if the opposite direction has no cars
+    // Otherwise, change direction if there are no cars in the current direction
       if ((current_direction == dir_left && (norm_right + emer_right) == 0) ||
           (current_direction == dir_right && (norm_left + emer_left) == 0)) {
         current_direction = (current_direction == dir_left) ? dir_right : dir_left;
       }
     }
 
-    current_direction = (current_direction == dir_left) ? dir_right : dir_left;
-    sema_up(&general_sema);
-    sema_up(&general_sema);
-    choose_and_place();
+    current_direction = (current_direction == dir_left) ? dir_right : dir_left; // Finally, change direction anyway
+    sema_up(&mutex_sema); // Clear the bridge
+    sema_up(&mutex_sema);
+    choose_and_place();   // Choose and place the next most appropriate car(s)
   }
 }
