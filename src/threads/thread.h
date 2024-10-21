846c25e24e42ebba93409664b76d59a80654d992
Ivan Shtarev <ishtarev@outlook.com>
2024-10-21 18:59:37 +0300

    handle sys_write sys_exit and implement exit_code print

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -82,6 +82,7 @@ typedef int tid_t;
    blocked state is on a semaphore wait list. */
 struct thread
   {
+   short exit_code;
     /* Owned by thread.c. */
     tid_t tid;                          /**< Thread identifier. */
     enum thread_status status;          /**< Thread state. */
