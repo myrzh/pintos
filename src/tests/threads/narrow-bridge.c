fb999ff73e3a1d1efc74835c240a5f227465b269
Ivan Shtarev <ishtarev@outlook.com>
2024-10-28 20:08:06 +0300

    add comments

--- src/tests/threads/narrow-bridge.c
+++ src/tests/threads/narrow-bridge.c
@@ -58,32 +58,40 @@ void narrow_bridge_init(void)
 void arrive_bridge(enum car_priority prio, enum car_direction dir)
 {    
     if (prio == car_emergency && now_crossing == 2) {
+        // If there is an emergency, but the bridge is full, we need to wait
         process_car(car_emergency, dir, arrive);
     } else if ((prio == car_normal && now_crossing == 2) || ((dir == dir_left) ? emer_left : emer_right) > 0) {
+        // If there is a normal car and the bridge is full, or there is an emergency car on the same side, we need to wait
         process_car(car_normal, dir, arrive);
     }
+    // If there is no need to wait, we can cross the bridge
     now_crossing++;
 }
 
 /** Called every time the car needs to exit the bridge */
 void exit_bridge(enum car_priority prio UNUSED, enum car_direction dir UNUSED)
 {
-    if (now_crossing > 0) {
+    if (now_crossing > 0) { // If there are cars on the bridge, let them go
         now_crossing--;
     }
-    if (now_crossing == 0) {
-        enum car_priority priorities[] = {car_emergency, car_emergency, car_normal, car_normal};
+    if (now_crossing == 0) { // If the bridge is free, let the next cars go
+        // Every combination of priorities and directions
+        // First emergency cars, then normal cars. Left is prior to right
+        enum car_priority priorities[] = {car_emergency, car_emergency, car_normal, car_normal}; // 
         enum car_direction directions[] = {dir_left, dir_right, dir_left, dir_right};
         int *counts[] = {&emer_left, &emer_right, &norm_left, &norm_right};
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 4; i++) { // For every combination of priorities and directions
             if (*counts[i] > 0) {
+                // If there are cars of this priority, let them go
                 process_car(priorities[i], directions[i], exit);
                 if (*counts[i] > 0) {
+                    // If there one more car of this priority, we need to send them together
                     process_car(priorities[i], directions[i], exit);
-                } else if (i < 2 && *counts[i + 2] > 0) { 
+                } else if (i < 2 && *counts[i + 2] > 0) {
+                    // If there are no emergencies, but there is a normal car, to send them together
                     process_car(car_normal, directions[i], exit);
                 }
-                break;
+                break; // We can send only two cars at a time
             }
         }
     }
