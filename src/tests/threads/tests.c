ce776cf6ccc6dd5cbdf28d3a5e463421e3e81a09
Ivan Shtarev <ishtarev@outlook.com>
2024-10-28 16:27:54 +0300

    prepare narrow_bridge tests

--- src/tests/threads/tests.c
+++ src/tests/threads/tests.c
@@ -2,11 +2,23 @@
 #include <debug.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "threads/malloc.h"
+
+enum test_args_type
+  {
+    test_no_args,
+    test_args_1uint,
+    test_args_2uint,
+    test_args_3uint,
+    test_args_4uint
+  };
 
 struct test 
   {
     const char *name;
     test_func *function;
+    enum test_args_type args_type;
   };
 
 static const struct test tests[] = 
@@ -38,25 +50,49 @@ static const struct test tests[] =
     {"mlfqs-nice-2", test_mlfqs_nice_2},
     {"mlfqs-nice-10", test_mlfqs_nice_10},
     {"mlfqs-block", test_mlfqs_block},
+    {"narrow-bridge", test_narrow_bridge, test_args_4uint},
   };
 
 static const char *test_name;
 
-/** Runs the test named NAME. */
+void call_test(const struct test* t, const char* name_args);
+
+/* Runs the test named NAME. */
 void
 run_test (const char *name) 
 {
   const struct test *t;
 
   for (t = tests; t < tests + sizeof tests / sizeof *tests; t++)
+  {
+    // Tests without args
     if (!strcmp (name, t->name))
       {
         test_name = name;
         msg ("begin");
-        t->function ();
+        call_test(t, name);
         msg ("end");
         return;
       }
+
+    // Tests with args
+    if (strstr(name, " ") && !strncmp(name, t->name, strstr(name, " ") - name ) )
+    {
+       size_t sz = strstr(name, " ") - name;
+       char* nm = malloc(sz + 1);
+       memcpy(nm, name, sz);
+       nm[sz] = 0;
+
+       test_name = nm;
+       msg ("begin");
+       call_test(t, name);
+       msg ("end");
+
+       free(nm);
+       return;
+    }
+  }
+
   PANIC ("no test named \"%s\"", name);
 }
 
@@ -100,3 +136,74 @@ pass (void)
   printf ("(%s) PASS\n", test_name);
 }
 
+static int check_args_count(unsigned int need, unsigned int cnt)
+{
+      if (cnt < need)
+      {
+          PANIC("invalid arguments count");
+          return 0;
+      }
+      return 1;
+}
+
+void call_test(const struct test* t, const char* name_args)
+{
+   if (t->args_type == test_no_args)
+   {
+      test_func* p = (test_func*) t->function;
+      p();
+   }
+   else if ((t->args_type >= test_args_1uint && t->args_type <= test_args_4uint) && strstr(name_args, " "))
+   {
+      const char* cargs = strstr(name_args, " ") + 1;
+      char* params[16] = { 0 };
+      char *token, *save_ptr, *args;
+      unsigned int cnt = 0;
+      size_t sz = sizeof(char) * (strlen(cargs) + 1);
+
+      // Copy args, parsing it
+      args = (char*) malloc(sz);
+      memcpy(args, cargs, sz);
+
+      for (token = strtok_r (args, " ", &save_ptr); token != NULL; token = strtok_r (NULL, " ", &save_ptr))
+      {
+          if (cnt < sizeof(params) / sizeof(params[0]))
+          {
+              params[cnt] = token;
+              cnt++;
+          }
+      }
+
+      if (t->args_type == test_args_1uint)
+      {
+          test_func_args_1uint* p = (test_func_args_1uint *) t->function;
+          check_args_count(1, cnt);
+          p((unsigned int) atoi(params[0]));
+      }
+      else if (t->args_type == test_args_2uint)
+      {
+          test_func_args_2uint* p = (test_func_args_2uint *) t->function;
+          check_args_count(2, cnt);
+          p((unsigned int) atoi(params[0]), (unsigned int) atoi(params[1]) );
+      }
+      else if (t->args_type == test_args_3uint)
+      {
+          test_func_args_3uint* p = (test_func_args_3uint *) t->function;
+          check_args_count(3, cnt);
+          p((unsigned int) atoi(params[0]), (unsigned int) atoi(params[1]), (unsigned int) atoi(params[2]) );
+      }
+      else if (t->args_type == test_args_4uint)
+      {
+          test_func_args_4uint* p = (test_func_args_4uint *) t->function;
+          check_args_count(3, cnt);
+          p((unsigned int) atoi(params[0]), (unsigned int) atoi(params[1]), (unsigned int) atoi(params[2]), (unsigned int) atoi(params[3]) );          
+      }
+
+      // Free args copy
+      free(args);
+   }
+   else
+   {
+       PANIC("invalid arguments");
+   }
+}
